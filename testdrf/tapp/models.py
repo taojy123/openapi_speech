3ffa51f36eb9351bb67cda793196548739c78936
taojy123 <taojy123@163.com>
2020-06-14 00:37:11 +0800

    差不多了

--- testdrf/tapp/models.py
+++ testdrf/tapp/models.py
@@ -5,7 +5,6 @@ class Product(models.Model):
     
     name = models.CharField(max_length=100, blank=True, default='abc', verbose_name='名称')
     price = models.IntegerField(default=0, verbose_name='价格')
-    description = models.TextField(blank=True)
 
 
 
